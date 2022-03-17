d14dbc4db33e7345ebede3121db197bd203c6f83
Metador - $Meta <95138869+Metador-SMARTBCH@users.noreply.github.com>
2022-03-17 05:11:02 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -229,7 +229,7 @@ function App() {
                 color: "var(--accent-text)",
               }}
             >
-              // {data.totalSupply} / {CONFIG.MAX_SUPPLY}
+              
             </s.TextTitle>
             <s.TextDescription
               style={{
